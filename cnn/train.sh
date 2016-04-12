28d1e520c16728b9faf3fe1ff116eca9bf4843f7
Zhi Xing <zxing01@syr.edu>
2016-04-12 17:25:28 -0400

    Add report skeleton.

--- cnn/train.sh
+++ cnn/train.sh
@@ -4,4 +4,4 @@ python train.py \
 --pos_file=../sentiment140/train.pos.txt \
 --neg_file=../sentiment140/train.neg.txt \
 --word2vec_file=./word2vec/model.ckpt-2265405 \
---vocab_file=./word2vec/vocab.txt \
+--vocab_file=./word2vec/vocab.txt
