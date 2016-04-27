cf7495009f1b24400566cbce460abf7e6f243cf9
Zhi Xing <zxing01@syr.edu>
2016-04-27 13:25:57 -0400

    Add readme file.

--- cnn/train.sh
+++ cnn/train.sh
@@ -5,6 +5,4 @@ python train.py \
 --neg_file=../sentiment140/train.neg.txt \
 --word2vec_file=./word2vec/model.ckpt-2265405 \
 --vocab_file=./word2vec/vocab.txt \
---filter_sizes="1,2,3,4" \
---num_epoch=50 \
 --checkpoint_dir=./checkpoints
